b9ca1f054c2b9b49cb75c1d81dd1536ec1e00ca0
echo <hujw77@gmail.com>
2024-04-16 15:35:28 +0800

    support ERC721Votes

--- src/DarwiniaCommunityDaoSBT.sol
+++ src/DarwiniaCommunityDaoSBT.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts@4.8.2/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.8.2/access/Ownable.sol";
-import "@openzeppelin/contracts@4.8.2/utils/Counters.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.6/token/ERC721/extensions/ERC721Votes.sol";
+import "@openzeppelin/contracts@4.9.6/access/Ownable2Step.sol";
+import "@openzeppelin/contracts@4.9.6/utils/cryptography/EIP712.sol";
+import "@openzeppelin/contracts@4.9.6/utils/Counters.sol";
 import "./IERC4906.sol";
 import "./IERC5192.sol";
 
@@ -20,7 +22,17 @@ import "./IERC5192.sol";
 /// 5. Metadata and image are pinned to ipfs.
 /// 6. Token uri metadata are changeable by contract owner.
 /// @custom:security-contact security@darwinia.network
-contract DarwiniaCommunityDaoSBT is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable, IERC4906, IERC5192 {
+contract DarwiniaCommunityDaoSBT is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    Ownable2Step,
+    EIP712,
+    ERC721Votes,
+    IERC4906,
+    IERC5192
+{
     using Counters for Counters.Counter;
 
     error ErrLocked();
@@ -32,15 +44,25 @@ contract DarwiniaCommunityDaoSBT is ERC721, ERC721Enumerable, ERC721URIStorage, 
     bool private constant LOCKED = true;
 
     // --- Auth ---
-    mapping (address => uint) public wards;
-    function rely(address guy) external onlyOwner { wards[guy] = 1; }
-    function deny(address guy) external onlyOwner { wards[guy] = 0; }
-    modifier auth {
+    mapping(address => uint256) public wards;
+
+    function rely(address guy) external onlyOwner {
+        wards[guy] = 1;
+    }
+
+    function deny(address guy) external onlyOwner {
+        wards[guy] = 0;
+    }
+
+    modifier auth() {
         require(wards[_msgSender()] == 1, "DDP/not-authorized");
         _;
     }
 
-    constructor(address dao) ERC721("Darwinia Community DAO Profile", "DCDP") {
+    constructor(address dao)
+        ERC721("Darwinia Community DAO Profile", "gDCDP")
+        EIP712("Darwinia Community DAO Profile", "1")
+    {
         wards[dao] = 1;
         _transferOwnership(dao);
     }
@@ -76,9 +98,16 @@ contract DarwiniaCommunityDaoSBT is ERC721, ERC721Enumerable, ERC721URIStorage, 
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
+    function _afterTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
     // Only contract owner could transfer/burn SBT
     function _isApprovedOrOwner(address spender, uint256) internal view override returns (bool) {
-        if(spender != owner()) revert ErrLocked();
+        if (spender != owner()) revert ErrLocked();
         return true;
     }
 
@@ -100,12 +129,7 @@ contract DarwiniaCommunityDaoSBT is ERC721, ERC721Enumerable, ERC721URIStorage, 
         return _base_uri;
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
@@ -114,13 +138,7 @@ contract DarwiniaCommunityDaoSBT is ERC721, ERC721Enumerable, ERC721URIStorage, 
         return LOCKED;
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return interfaceId == type(IERC5192).interfaceId ||
-               super.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return interfaceId == type(IERC5192).interfaceId || super.supportsInterface(interfaceId);
     }
 }
