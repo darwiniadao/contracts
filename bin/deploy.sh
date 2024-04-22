949db435739794ca4b32ac624cf4c0efd30d2c46
echo <hujw77@gmail.com>
2024-04-22 10:31:34 +0800

    deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,18 +2,17 @@
 
 set -eo pipefail
 
-# multisig=0x8683A0ED2bAdb1F3AaBeA6686d78649F0Da774A0
+multisig=0x8683A0ED2bAdb1F3AaBeA6686d78649F0Da774A0
 deployer=0x0f14341A7f464320319025540E8Fe48Ad0fe5aec
-multisig=$deployer
 
-minDelay=60
+minDelay=86400
 az=0x0000000000000000000000000000000000000000
 
 nonce=$(seth nonce $deployer)
 nonce=$(( nonce + 2))
 dcdao=$(dapp address $deployer $nonce)
 
-token=$(dapp create DarwiniaCommunityDaoSBT $deployer)
+token=$(dapp create DarwiniaCommunityDaoSBT $multisig)
 timelock=$(dapp create TimelockController $minDelay [$dcdao] [$az] $multisig)
 dapp create DCDAO $token $timelock
 echo "token: $token"
