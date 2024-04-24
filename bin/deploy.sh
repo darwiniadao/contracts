3bc3815cc2142fa2a11b5483361ceb7c88b03766
echo <hujw77@gmail.com>
2024-04-24 14:26:45 +0800

    deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,10 +14,12 @@ dcdao=$(dapp address $deployer $nonce)
 
 token=$(dapp create DarwiniaCommunityDaoSBT $multisig)
 
-wads=[$multisig.owners()]
+wads=[0xa6B5EC845C8446A350a50bb58363c241ab7990b5,0x755d783fd1Cb2133A1ae25c2c1D73947Cb5DEb79,0xA4bE619E8C0E3889f5fA28bb0393A4862Cad35ad,0x7aE77149ed38c5dD313e9069d790Ce7085caf0A6,0x3ba1F5979Ac9cED405419733aCD5887e4F5F2FD7]
+
 timelock=$(dapp create TimelockController $minDelay [$dcdao] $wads $az)
 dapp create DCDAO $token $timelock
-seth send $token "rely(address)" $dcdao
+
+# seth send $token "rely(address)" $dcdao
 
 echo "token: $token"
 echo "timelock: $timelock"
